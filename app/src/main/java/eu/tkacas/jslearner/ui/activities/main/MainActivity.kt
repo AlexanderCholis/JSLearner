ce2fc66196b744b3051d641b0e2607b4555b6791
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-05-10 20:06:50 +0300

    Change initial Screen

--- app/src/main/java/eu/tkacas/jslearner/ui/activities/main/MainActivity.kt
+++ app/src/main/java/eu/tkacas/jslearner/ui/activities/main/MainActivity.kt
@@ -19,8 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //Navigation()
-                    ExperienceLevelScreen()
+                    Navigation()
                 }
             }
         }
