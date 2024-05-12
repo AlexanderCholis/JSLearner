dd19dcb50bd3c279a5ae1abfa947ecc296e11ddd
Alexander Cholis <66769337+AlexanderCholis@users.noreply.github.com>
2024-05-12 22:22:38 +0300

    Logo successfully changed

--- app/src/main/java/eu/tkacas/jslearner/ui/activities/welcome/screens/WelcomeScreen.kt
+++ app/src/main/java/eu/tkacas/jslearner/ui/activities/welcome/screens/WelcomeScreen.kt
@@ -16,27 +16,28 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import eu.tkacas.jslearner.R
+import androidx.compose.ui.Modifier
 
 
 
 @Composable
 fun WelcomeScreen() {
     Column(
-        modifier = androidx.compose.ui.Modifier
+        modifier = Modifier
             .fillMaxSize()
             .padding(top = 30.dp, start = 32.dp, end = 32.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Image(
-            painter = painterResource(id = R.drawable.application),
+            painter = painterResource(id = R.drawable.jslearner_transparent),
             contentDescription = "Welcome Image"
         )
-        Spacer(modifier = androidx.compose.ui.Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = stringResource(id = R.string.welcome_text),
             textAlign = TextAlign.Center
         )
-        Spacer(modifier = androidx.compose.ui.Modifier.height(60.dp))
+        Spacer(modifier = Modifier.height(60.dp))
         Text(
             text = stringResource(id = R.string.creators),
             textAlign = TextAlign.Center
