bac36e976fa608997c14b8216fcbb861aacfbde7
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-14 11:36:48 +0300

    - edit LoginFormState to add more cases from the db response

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/state/LoginFormState.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/state/LoginFormState.kt
@@ -4,5 +4,8 @@ data class LoginFormState (
     val email: String = "",
     val emailError: String? = null,
     val password: String = "",
-    val passwordError: String? = null
+    val passwordError: String? = null,
+    val isLoading: Boolean = false,
+    val isDataValid: Boolean = false,
+    val isDataValidError: String? = null
 )
