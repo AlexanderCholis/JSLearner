1cb173fbc55fb919d8da4664f40ce62e8e55c05a
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-14 11:34:26 +0300

    - add parameter for navigateToSignUp -> navController

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/welcome/navigation/actions/ILoginActions.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/welcome/navigation/actions/ILoginActions.kt
@@ -1,5 +1,7 @@
 package eu.tkacas.jslearner.presentation.ui.activity.welcome.navigation.actions
 
+import androidx.navigation.NavController
+
 interface ILoginActions {
-    fun navigateToSignUp()
+    fun navigateToSignUp(navController: NavController)
 }
