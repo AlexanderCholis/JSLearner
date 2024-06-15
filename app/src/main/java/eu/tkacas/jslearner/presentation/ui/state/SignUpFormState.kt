6e74f43ae443d1c85f4852120d77e4d6bc046131
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-06-15 17:47:57 +0300

    Update SignUpFormState.kt

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/state/SignUpFormState.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/state/SignUpFormState.kt
@@ -1,5 +1,7 @@
 package eu.tkacas.jslearner.presentation.ui.state
 
+import com.google.firebase.auth.FirebaseUser
+
 data class SignUpFormState(
     val firstName: String = "",
     val firstNameError: String? = null,
@@ -10,5 +12,6 @@ data class SignUpFormState(
     val password: String = "",
     val passwordError: String? = null,
     val acceptedTerms: Boolean = false,
-    val termsError: String? = null
+    val termsError: String? = null,
+    val submitStatus: Result<FirebaseUser>? = null
 )
