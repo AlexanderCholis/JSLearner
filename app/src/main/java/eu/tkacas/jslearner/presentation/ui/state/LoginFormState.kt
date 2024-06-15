27d42add9e55ed67f801eaa503704505023379fe
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-15 18:51:46 +0300

    - delete isLoading, isDataValid, isDataValidError

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/state/LoginFormState.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/state/LoginFormState.kt
@@ -4,8 +4,5 @@ data class LoginFormState (
     val email: String = "",
     val emailError: String? = null,
     val password: String = "",
-    val passwordError: String? = null,
-    val isLoading: Boolean = false,
-    val isDataValid: Boolean = false,
-    val isDataValidError: String? = null
+    val passwordError: String? = null
 )
