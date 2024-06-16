45d93f601c42a6e9a1474351906cc159a6f71b24
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-06-16 14:31:21 +0300

    Create ExperienceLevelUiItem which is the previous ExperienceLevelItem and add the new ExperienceLevelItem

--- app/src/main/java/eu/tkacas/jslearner/domain/entity/experience/ExperienceLevelUiItem.kt
+++ app/src/main/java/eu/tkacas/jslearner/domain/entity/experience/ExperienceLevelUiItem.kt
@@ -1,5 +1,7 @@
 package eu.tkacas.jslearner.domain.entity.experience
 
-data class ExperienceLevelItem(
+data class ExperienceLevelUiItem(
+    val image: Int,
+    val text: Int,
     val level: ExperienceLevel
 )
