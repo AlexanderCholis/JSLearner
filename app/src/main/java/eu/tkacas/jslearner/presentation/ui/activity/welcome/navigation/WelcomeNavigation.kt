ac1655e81f6f1886a03e5fbd397420992ce057c3
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-06-16 14:42:54 +0300

    Add ExperienceLevelViewModel into WelcomeActivity. Add navigation to ExperienceLevelScreen

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/welcome/navigation/WelcomeNavigation.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/welcome/navigation/WelcomeNavigation.kt
@@ -4,11 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import eu.tkacas.jslearner.presentation.ui.activity.welcome.screens.ExperienceLevelScreen
 import eu.tkacas.jslearner.presentation.ui.activity.welcome.screens.PrivacyPolicyScreen
 import eu.tkacas.jslearner.presentation.ui.activity.welcome.screens.TermsAndConditionsScreen
 import eu.tkacas.jslearner.presentation.ui.activity.welcome.screens.WelcomeScreen
 import eu.tkacas.jslearner.presentation.ui.activity.welcome.screens.auth.LoginScreen
 import eu.tkacas.jslearner.presentation.ui.activity.welcome.screens.auth.SignUpScreen
+import eu.tkacas.jslearner.presentation.viewmodel.welcome.ExperienceLevelViewModel
 import eu.tkacas.jslearner.presentation.viewmodel.welcome.auth.LoginViewModel
 import eu.tkacas.jslearner.presentation.viewmodel.welcome.auth.SignUpViewModel
 
@@ -16,7 +18,8 @@ import eu.tkacas.jslearner.presentation.viewmodel.welcome.auth.SignUpViewModel
 @Composable
 internal fun WelcomeNavigation(
     loginViewModel: LoginViewModel,
-    signUpViewModel: SignUpViewModel
+    signUpViewModel: SignUpViewModel,
+    experienceLevelViewModel: ExperienceLevelViewModel
 ){
     val navController = rememberNavController()
     NavHost(
@@ -28,5 +31,6 @@ internal fun WelcomeNavigation(
         composable("signUp") { SignUpScreen(navController = navController, viewModel = signUpViewModel) }
         composable("termsAndConditions") { TermsAndConditionsScreen(navController = navController) }
         composable("privacyPolicy") { PrivacyPolicyScreen(navController = navController) }
+        composable("experienceLevel") { ExperienceLevelScreen(navController = navController, viewModel = experienceLevelViewModel) }
     }
 }
