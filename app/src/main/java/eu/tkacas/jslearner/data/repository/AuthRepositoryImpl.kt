7fc65fac20516b37c98e247cc13a31a63986759d
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-06-16 11:07:43 +0300

    Update signup method to also save User into Realtime Database

--- app/src/main/java/eu/tkacas/jslearner/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/eu/tkacas/jslearner/data/repository/AuthRepositoryImpl.kt
@@ -1,14 +1,18 @@
 package eu.tkacas.jslearner.data.repository
 
+import android.annotation.SuppressLint
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.database.FirebaseDatabase
 import eu.tkacas.jslearner.data.await
 import eu.tkacas.jslearner.domain.repository.AuthRepository
 import eu.tkacas.jslearner.domain.Result
+import eu.tkacas.jslearner.domain.entity.User
 
 class AuthRepositoryImpl (
-    private val firebaseAuth: FirebaseAuth
+    private val firebaseAuth: FirebaseAuth,
+    private val firebaseDatabase: FirebaseDatabase
 ): AuthRepository {
     override val currentUser: FirebaseUser?
         get() = firebaseAuth.currentUser
@@ -23,10 +27,16 @@ class AuthRepositoryImpl (
         }
     }
 
+    @SuppressLint("RestrictedApi")
     override suspend fun signup(firstName: String, lastName: String, email: String, password: String): Result<FirebaseUser> {
         return try {
             val result = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
             result.user?.updateProfile(UserProfileChangeRequest.Builder().setDisplayName("$firstName $lastName").build())?.await()
+
+            // Save user to Firebase Realtime Database
+            val user = User(firstName, lastName, email)
+            firebaseDatabase.getReference("users").child(result.user!!.uid).setValue(user)
+
             return Result.Success(result.user!!)
         } catch (e: Exception) {
             e.printStackTrace()
