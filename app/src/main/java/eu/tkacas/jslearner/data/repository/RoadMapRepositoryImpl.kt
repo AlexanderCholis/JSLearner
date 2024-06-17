937ae2dc4798e80e5ee9d038e1049adf80e0caa0
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-17 17:43:34 +0300

    add repository implementation of the RoadMapRepository in domain layer

---
+++ app/src/main/java/eu/tkacas/jslearner/data/repository/RoadMapRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package eu.tkacas.jslearner.data.repository
+
+import eu.tkacas.jslearner.data.model.Course
+import eu.tkacas.jslearner.data.model.Lesson
+import eu.tkacas.jslearner.data.model.Question
+import eu.tkacas.jslearner.data.source.remote.RoadMapDataSource
+import eu.tkacas.jslearner.domain.repository.RoadMapRepository
+
+class RoadMapRepositoryImpl(private val dataSource: RoadMapDataSource) : RoadMapRepository {
+
+    override suspend fun getCourses() : List<Course> {
+        return dataSource.getCourses()
+    }
+
+    override suspend fun getLessons(courseId: String): List<Lesson> {
+        return dataSource.getLessons(courseId)
+    }
+
+    override suspend fun getQuestions(courseId: String, lessonId: String) : List<Question> {
+        return dataSource.getQuestions(courseId, lessonId)
+    }
+
+    override suspend fun getUserCompletedCourses(userId: String): Map<String, List<String>> {
+        return dataSource.getUserCompletedCourses(userId)
+    }
+}
