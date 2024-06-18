06edb8235095d67f05716340057b81898e3e9280
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-18 13:31:58 +0300

    - add course level to course data class

--- app/src/main/java/eu/tkacas/jslearner/data/model/Course.kt
+++ app/src/main/java/eu/tkacas/jslearner/data/model/Course.kt
@@ -2,6 +2,7 @@ package eu.tkacas.jslearner.data.model
 
 data class Course(
     val id: String = "",
+    val level: CourseLevel = CourseLevel.BEGINNER,
     val title: String = "",
     val descriptionShort: String = "",
     val descriptionLong: String = ""
