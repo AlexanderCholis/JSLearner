bb63813d07cfff0a9ad2392d76f936cae79ab41b
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-20 19:29:42 +0300

    - create a new repository for the ExploringPathScreen

---
+++ app/src/main/java/eu/tkacas/jslearner/data/repository/ExploringPathRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package eu.tkacas.jslearner.data.repository
+
+import eu.tkacas.jslearner.data.model.Course
+import eu.tkacas.jslearner.data.model.CourseLevel
+import eu.tkacas.jslearner.data.source.remote.FirestoreDataSource
+import eu.tkacas.jslearner.domain.model.experience.ExperienceLevel
+import eu.tkacas.jslearner.domain.repository.ExploringPathRepository
+
+class ExploringPathRepositoryImpl(private val dataSource: FirestoreDataSource) : ExploringPathRepository {
+    override suspend fun getCoursesBasedOnExperience(experienceLevel: ExperienceLevel): List<Course> {
+        return when (experienceLevel) {
+            ExperienceLevel.NO_EXPERIENCE -> dataSource.getCoursesBasedOnLevel(CourseLevel.BEGINNER)
+            ExperienceLevel.SOME_EXPERIENCE -> dataSource.getCoursesBasedOnLevel(CourseLevel.INTERMEDIATE)
+            ExperienceLevel.A_LOT_OF_EXPERIENCE -> dataSource.getCoursesBasedOnLevel(CourseLevel.ADVANCED)
+        }
+    }
+}
---
+++ app/src/main/java/eu/tkacas/jslearner/domain/repository/ExploringPathRepository.kt
@@ -0,0 +1,8 @@
+package eu.tkacas.jslearner.domain.repository
+
+import eu.tkacas.jslearner.data.model.Course
+import eu.tkacas.jslearner.domain.model.experience.ExperienceLevel
+
+interface ExploringPathRepository {
+    suspend fun getCoursesBasedOnExperience(experienceLevel: ExperienceLevel): List<Course>
+}
