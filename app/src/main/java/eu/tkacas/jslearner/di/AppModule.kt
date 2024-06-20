d02eed1e880ddfdf0877cf5166ce82940d5194fb
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-20 19:30:30 +0300

    - reorganize the AppModule and AppModuleImpl - add comments

--- app/src/main/java/eu/tkacas/jslearner/di/AppModule.kt
+++ app/src/main/java/eu/tkacas/jslearner/di/AppModule.kt
@@ -4,8 +4,10 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 import eu.tkacas.jslearner.domain.repository.AuthRepository
+import eu.tkacas.jslearner.domain.repository.ExploringPathRepository
 import eu.tkacas.jslearner.domain.repository.RoadMapRepository
 import eu.tkacas.jslearner.domain.usecase.main.roadmap.GetRoadMapUseCase
+import eu.tkacas.jslearner.domain.usecase.welcome.exploringpath.GetCoursesBasedOnExperienceUseCase
 import eu.tkacas.jslearner.domain.usecase.welcome.validateregex.ValidateEmail
 import eu.tkacas.jslearner.domain.usecase.welcome.validateregex.ValidateFirstName
 import eu.tkacas.jslearner.domain.usecase.welcome.validateregex.ValidateLastName
@@ -13,18 +15,25 @@ import eu.tkacas.jslearner.domain.usecase.welcome.validateregex.ValidatePassword
 import eu.tkacas.jslearner.domain.usecase.welcome.validateregex.ValidateTerms
 
 interface AppModule {
+
+    // Firebase
     fun getFirebaseAuth(): FirebaseAuth
     fun getFirebaseDatabase(): FirebaseDatabase
     fun getFirestoreDatabase(): FirebaseFirestore
 
+    // For the SignIn and SignUp screens
     val authRepository: AuthRepository
-
     val validateFirstName: ValidateFirstName
     val validateLastName: ValidateLastName
     val validateEmail: ValidateEmail
     val validatePassword: ValidatePassword
     val validateTerms: ValidateTerms
 
+    // For the RoadMapScreen
     val roadMapRepository: RoadMapRepository
     val getRoadMapUseCase: GetRoadMapUseCase
+
+    // For the ExploringPathScreen
+    val exploringPathRepository: ExploringPathRepository
+    val getCoursesBasedOnExperienceUseCase: GetCoursesBasedOnExperienceUseCase
 }
--- app/src/main/java/eu/tkacas/jslearner/di/AppModuleImpl.kt
+++ app/src/main/java/eu/tkacas/jslearner/di/AppModuleImpl.kt
@@ -5,11 +5,14 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 import eu.tkacas.jslearner.data.repository.AuthRepositoryImpl
+import eu.tkacas.jslearner.data.repository.ExploringPathRepositoryImpl
 import eu.tkacas.jslearner.data.repository.RoadMapRepositoryImpl
-import eu.tkacas.jslearner.data.source.remote.RoadMapDataSource
+import eu.tkacas.jslearner.data.source.remote.FirestoreDataSource
 import eu.tkacas.jslearner.domain.repository.AuthRepository
+import eu.tkacas.jslearner.domain.repository.ExploringPathRepository
 import eu.tkacas.jslearner.domain.repository.RoadMapRepository
 import eu.tkacas.jslearner.domain.usecase.main.roadmap.GetRoadMapUseCase
+import eu.tkacas.jslearner.domain.usecase.welcome.exploringpath.GetCoursesBasedOnExperienceUseCase
 import eu.tkacas.jslearner.domain.usecase.welcome.validateregex.ValidateEmail
 import eu.tkacas.jslearner.domain.usecase.welcome.validateregex.ValidateFirstName
 import eu.tkacas.jslearner.domain.usecase.welcome.validateregex.ValidateLastName
@@ -19,21 +22,22 @@ import eu.tkacas.jslearner.domain.usecase.welcome.validateregex.ValidateTerms
 class AppModuleImpl(
     private val appContext: Context
 ) : AppModule {
+
+    // Firebase
     override fun getFirebaseAuth(): FirebaseAuth {
         return FirebaseAuth.getInstance()
     }
-
     override fun getFirebaseDatabase(): FirebaseDatabase {
         return FirebaseDatabase.getInstance()
     }
 
+    // Firestore
+    private val dataSource = FirestoreDataSource(getFirestoreDatabase())
     override fun getFirestoreDatabase(): FirebaseFirestore {
         return FirebaseFirestore.getInstance()
     }
 
-
-    private val dataSource = RoadMapDataSource(getFirestoreDatabase())
-
+    // For the RoadMapScreen
     override val roadMapRepository: RoadMapRepository by lazy {
         RoadMapRepositoryImpl(dataSource)
     }
@@ -41,28 +45,31 @@ class AppModuleImpl(
         GetRoadMapUseCase(roadMapRepository)
     }
 
+    // For the ExploringPathScreen
+    override val exploringPathRepository: ExploringPathRepository by lazy {
+        ExploringPathRepositoryImpl(dataSource)
+    }
+    override val getCoursesBasedOnExperienceUseCase: GetCoursesBasedOnExperienceUseCase by lazy {
+        GetCoursesBasedOnExperienceUseCase(exploringPathRepository)
+    }
 
 
+    // For the SignIn and SignUp screens
     override val authRepository: AuthRepository by lazy {
         AuthRepositoryImpl(getFirebaseAuth(), getFirebaseDatabase())
     }
-
     override val validateFirstName: ValidateFirstName by lazy {
         ValidateFirstName()
     }
-
     override val validateLastName: ValidateLastName by lazy {
         ValidateLastName()
     }
-
     override val validateEmail: ValidateEmail by lazy {
         ValidateEmail()
     }
-
     override val validatePassword: ValidatePassword by lazy {
         ValidatePassword()
     }
-
     override val validateTerms: ValidateTerms by lazy {
         ValidateTerms()
     }
