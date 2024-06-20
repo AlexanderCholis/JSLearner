67d323bb588463ada34b1739d735a211b9dedc79
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-06-20 23:23:48 +0300

    Rename PathCardUiItem to CourseShort

---
+++ app/src/main/java/eu/tkacas/jslearner/domain/model/CourseShort.kt
@@ -0,0 +1,6 @@
+package eu.tkacas.jslearner.domain.model
+
+data class CourseShort(
+    val title: String,
+    val description: String
+)
--- app/src/main/java/eu/tkacas/jslearner/presentation/model/PathCardUiItem.kt
+++
@@ -1,6 +0,0 @@
-package eu.tkacas.jslearner.presentation.model
-
-data class PathCardUiItem(
-    val title: String,
-    val description: String
-)
