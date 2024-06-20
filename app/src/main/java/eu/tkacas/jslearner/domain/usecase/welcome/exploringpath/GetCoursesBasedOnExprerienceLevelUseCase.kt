8633823d4a793428ecbd7199c6c09052b791e3d5
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-06-20 23:26:55 +0300

    Get the courses from firestore based on the selected experienceLevel

--- app/src/main/java/eu/tkacas/jslearner/domain/usecase/welcome/exploringpath/GetCoursesBasedOnExprerienceLevelUseCase.kt
+++ app/src/main/java/eu/tkacas/jslearner/domain/usecase/welcome/exploringpath/GetCoursesBasedOnExprerienceLevelUseCase.kt
@@ -3,9 +3,23 @@ package eu.tkacas.jslearner.domain.usecase.welcome.exploringpath
 import eu.tkacas.jslearner.data.model.Course
 import eu.tkacas.jslearner.domain.model.experience.ExperienceLevel
 import eu.tkacas.jslearner.domain.repository.ExploringPathRepository
+import eu.tkacas.jslearner.domain.model.CourseShort
 
 class GetCoursesBasedOnExperienceUseCase(private val exploringPathRepository: ExploringPathRepository) {
-    suspend fun execute(experienceLevel: ExperienceLevel): List<Course> {
-        return exploringPathRepository.getCoursesBasedOnExperience(experienceLevel)
+    suspend fun execute(experienceLevel: ExperienceLevel): MutableList<CourseShort> {
+        val courses = exploringPathRepository.getCoursesBasedOnExperience(experienceLevel)
+
+        val coursesList = mutableListOf<CourseShort>()
+
+        courses.forEach {
+            coursesList.add(
+                CourseShort(
+                    title = it.title,
+                    description = it.descriptionShort
+                )
+            )
+        }
+
+        return coursesList
     }
 }
--- app/src/main/java/eu/tkacas/jslearner/presentation/viewmodel/welcome/ExploringPathViewModel.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/viewmodel/welcome/ExploringPathViewModel.kt
@@ -1,29 +1,31 @@
 package eu.tkacas.jslearner.presentation.viewmodel.welcome
 
 import androidx.lifecycle.ViewModel
-import eu.tkacas.jslearner.data.model.Course
+import androidx.lifecycle.viewModelScope
+import eu.tkacas.jslearner.domain.Result
 import eu.tkacas.jslearner.domain.model.experience.ExperienceLevel
 import eu.tkacas.jslearner.domain.repository.ExploringPathRepository
 import eu.tkacas.jslearner.domain.usecase.welcome.exploringpath.GetCoursesBasedOnExperienceUseCase
-import eu.tkacas.jslearner.presentation.model.PathCardUiItem
+import eu.tkacas.jslearner.domain.model.CourseShort
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ExploringPathViewModel(
-    exploringPathRepository: ExploringPathRepository,
     private val getCoursesBasedOnExperienceUseCase: GetCoursesBasedOnExperienceUseCase
 ): ViewModel() {
 
-    private val _coursesUiList = mutableListOf<PathCardUiItem>()
-    val coursesUiList: List<PathCardUiItem> = _coursesUiList
+    private val _exploringPathState = MutableStateFlow<Result<List<CourseShort>>?>(null)
+    val exploringPathState: StateFlow<Result<List<CourseShort>>?> = _exploringPathState
 
-    suspend fun returnCourses(experienceLevel: ExperienceLevel) {
-        val coursesList = getCoursesBasedOnExperienceUseCase.execute(experienceLevel)
-        coursesList.forEach {
-            _coursesUiList.add(
-                PathCardUiItem(
-                    title = it.title,
-                    description = it.descriptionShort
-                )
-            )
+    fun returnCourses(experienceLevel: ExperienceLevel) = viewModelScope.launch {
+        try {
+            _exploringPathState.value = Result.Loading
+
+            val courses = getCoursesBasedOnExperienceUseCase.execute(experienceLevel)
+            _exploringPathState.value = Result.Success(courses)
+        } catch (e: Exception) {
+            _exploringPathState.value = Result.Error(e)
         }
     }
 }
