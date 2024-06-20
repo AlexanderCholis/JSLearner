972561fd5d9d2150391e0f653bcb53edfbf6f737
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-06-20 20:14:29 +0300

    Update PathModuleCard

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/component/CardComponents.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/component/CardComponents.kt
@@ -24,7 +24,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -136,9 +137,9 @@ fun LearningReasonCard(
 @Composable
 fun PathModuleCard(
     moduleTitleText : String,
-    moduleDescriptionText : String,
-    isExpanded: MutableState<Boolean>
+    moduleDescriptionText : String
 ) {
+    val isExpanded = remember { mutableStateOf(false) }
     Card(
         modifier = Modifier
             .fillMaxWidth(),
