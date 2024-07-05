8fef791ec05bf66e52aa96e3c69851b56cdcc4fe
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-05 18:55:35 +0300

    - create user class in data layer

---
+++ app/src/main/java/eu/tkacas/jslearner/data/model/User.kt
@@ -0,0 +1,21 @@
+package eu.tkacas.jslearner.data.model
+
+import eu.tkacas.jslearner.domain.model.experience.ExperienceLevel
+
+data class UserFirestore(
+    val uid: String = "",
+    val firstName: String = "",
+    val lastName: String = "",
+    val dateRegistered: String = "",
+    val reasonOfUsingTheApp: String = "",
+    val profileCompleted: Boolean = false
+)
+
+data class UserFirebase(
+    val experienceLevel: ExperienceLevel = ExperienceLevel.NO_EXPERIENCE,
+    val experienceScore: Int = 0,
+    val currentCourseId: String = "",
+    val currentLessonId: String = "",
+    val highScoreDaysInARow: Int = 0,
+    val highScoreCorrectAnswersInARow: Int = 0
+)
