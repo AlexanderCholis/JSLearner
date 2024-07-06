c03a88135d9a1758ce26f79ced0c2bdee0ee1aaf
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-06 11:47:46 +0300

    - add updateUserProfile - add updateUserStats

--- app/src/main/java/eu/tkacas/jslearner/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/eu/tkacas/jslearner/data/repository/AuthRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package eu.tkacas.jslearner.data.repository
 
+import android.util.Log
 import com.google.firebase.auth.FirebaseUser
+import eu.tkacas.jslearner.data.model.UserFirebase
 import eu.tkacas.jslearner.data.model.UserFirestore
 import eu.tkacas.jslearner.data.source.remote.FirebaseDataSource
 import eu.tkacas.jslearner.data.source.remote.FirestoreDataSource
@@ -20,11 +22,30 @@ class AuthRepositoryImpl (
         val result = firebaseDataSource.signup(email, password)
         if (result is Result.Success) {
             val uid = result.result.uid
-            val user = UserFirestore(firstName, lastName, dateRegistered = getCurrentDate(), profileCompleted = false)
+            val user = UserFirestore(firstName, lastName, dateRegistered = getCurrentDate(), isProfileCompleted = false)
             firestoreDataSource.setUserProfile(uid, user)
         }
         return result
     }
+
+    suspend fun updateUserProfile(userId: String, reasonOfUsingTheApp: String, profileCompleted: Boolean) {
+        try { //Check for overwriting the user profile
+            val user = UserFirestore(reasonOfUsingTheApp = reasonOfUsingTheApp, isProfileCompleted = profileCompleted)
+            firestoreDataSource.setUserProfile(userId, user)
+        } catch (e: Exception) {
+            Log.w("AuthRepositoryImpl", "Error updating user profile.", e)
+        }
+    }
+
+    suspend fun updateUserStats(userId: String, user: UserFirebase) {
+        try {
+            firebaseDataSource.setUserStats(userId, user)
+        } catch (e: Exception) {
+            Log.w("AuthRepositoryImpl", "Error updating user stats.", e)
+        }
+}
+
+
 
     private fun getCurrentDate(): String {
         return System.currentTimeMillis().toString()
