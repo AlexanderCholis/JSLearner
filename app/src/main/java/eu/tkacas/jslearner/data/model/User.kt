c8c8a5f542aa441f93bf40148cda822374c4fbb0
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-07 01:55:11 +0300

    - add toMap for the UserFirebase and UserFirestore properties

--- app/src/main/java/eu/tkacas/jslearner/data/model/User.kt
+++ app/src/main/java/eu/tkacas/jslearner/data/model/User.kt
@@ -2,6 +2,7 @@ package eu.tkacas.jslearner.data.model
 
 import eu.tkacas.jslearner.domain.model.experience.ExperienceLevel
 import eu.tkacas.jslearner.domain.model.learningreason.LearningReason
+import kotlin.collections.mapOf
 
 data class UserFirestore(
     val firstName: String? = null,
@@ -10,7 +11,18 @@ data class UserFirestore(
     val learningReason: LearningReason? = null,
     val profileCompleted: Boolean? = false,
     val coursesCompleted: List<Lesson>? = null
-)
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "first_name" to firstName,
+            "last_name" to lastName,
+            "date_registered" to dateRegistered,
+            "learning_reason" to learningReason?.name,
+            "profile_completed" to profileCompleted,
+            "courses_completed" to coursesCompleted?.map { it.id }
+        )
+    }
+}
 
 data class UserFirebase(
     val experienceLevel: ExperienceLevel? = null,
@@ -19,4 +31,15 @@ data class UserFirebase(
     val currentLessonId: String? = null,
     val highScoreDaysInARow: Int? = null,
     val highScoreCorrectAnswersInARow: Int? = null
-)
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "experience_level" to experienceLevel?.name,
+            "experience_score" to experienceScore,
+            "current_course_id" to currentCourseId,
+            "current_lesson_id" to currentLessonId,
+            "high_score_days_in_a_row" to highScoreDaysInARow,
+            "high_score_correct_answers_in_a_row" to highScoreCorrectAnswersInARow
+        )
+    }
+}
