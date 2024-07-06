1778ace0be69d0fd54251989e0a9c1237837ab6a
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-06 17:40:07 +0300

    - add the rest of the features for UserFirebase stats

--- app/src/main/java/eu/tkacas/jslearner/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/eu/tkacas/jslearner/data/repository/AuthRepositoryImpl.kt
@@ -30,14 +30,39 @@ class AuthRepositoryImpl (
         return result
     }
 
-    override suspend fun updateUserProfile(learningReason: LearningReason, profileCompleted: Boolean, epxerienceLevel: ExperienceLevel) {
+    override suspend fun updateUserProfile(learningReason: LearningReason, profileCompleted: Boolean, experienceLevel: ExperienceLevel) {
         try {
             val uid = currentUser?.uid ?: return
             // Update the user profile in Firestore
             val user = UserFirestore(learningReason = learningReason, profileCompleted = profileCompleted)
             firestoreDataSource.updateUserProfile(uid, user)
             // Update the user stats in Firebase
-            val userStats = UserFirebase(experienceLevel = epxerienceLevel)
+            val userStats = when (experienceLevel) { // TODO: Update the experienceScore based on the experienceLevel
+                ExperienceLevel.NO_EXPERIENCE -> UserFirebase(
+                    experienceLevel = experienceLevel,
+                    experienceScore = 0,
+                    currentCourseId = "01",
+                    currentLessonId = "01",
+                    highScoreDaysInARow = 0,
+                    highScoreCorrectAnswersInARow = 0
+                    )
+                ExperienceLevel.SOME_EXPERIENCE -> UserFirebase(
+                    experienceLevel = experienceLevel,
+                    experienceScore = 500,
+                    currentCourseId = "04",
+                    currentLessonId = "01",
+                    highScoreDaysInARow = 0,
+                    highScoreCorrectAnswersInARow = 0
+                )
+                ExperienceLevel.A_LOT_OF_EXPERIENCE -> UserFirebase(
+                    experienceLevel = experienceLevel,
+                    experienceScore = 1000,
+                    currentCourseId = "08",
+                    currentLessonId = "01",
+                    highScoreDaysInARow = 0,
+                    highScoreCorrectAnswersInARow = 0
+                )
+            }
             firebaseDataSource.setUserStats(uid, userStats)
         } catch (e: Exception) {
             Log.w("AuthRepositoryImpl", "Error updating user profile.", e)
