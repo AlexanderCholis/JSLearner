d8a4f30834692a9d885a292e6ff6d2a77093de38
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-07 15:10:57 +0300

    - move file

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/main/screens/CoursesPathScreen.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/main/screens/CoursesPathScreen.kt
@@ -1,4 +1,4 @@
-package eu.tkacas.jslearner.presentation.ui.activity.welcome.screens
+package eu.tkacas.jslearner.presentation.ui.activity.main.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
