c026f0eae823989058c334c75596009647c46ce9
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-07 03:28:51 +0300

    - update the function

--- app/src/main/java/eu/tkacas/jslearner/domain/usecase/welcome/exploringpath/UpdateUserProfileUseCase.kt
+++ app/src/main/java/eu/tkacas/jslearner/domain/usecase/welcome/exploringpath/UpdateUserProfileUseCase.kt
@@ -5,7 +5,27 @@ import eu.tkacas.jslearner.domain.model.learningreason.LearningReason
 import eu.tkacas.jslearner.domain.repository.AuthRepository
 
 class UpdateUserProfileUseCase(private val authRepository: AuthRepository) {
-    suspend fun execute(learningReason: LearningReason, profileCompleted: Boolean, experienceLevel: ExperienceLevel) {
-        authRepository.updateUserProfile(learningReason, profileCompleted, experienceLevel)
+    suspend fun execute(
+        firstName: String?,
+        lastName: String?,
+        experienceScore: Int?,
+        learningReason: LearningReason?,
+        profileCompleted: Boolean?,
+        experienceLevel: ExperienceLevel?,
+        lessonsCompleted: List<String>?,
+        highScoreDaysInARow: Int?,
+        highScoreCorrectAnswersInARow: Int?
+    ) {
+        authRepository.updateUserProfile(
+            firstName = firstName,
+            lastName = lastName,
+            experienceScore = experienceScore,
+            learningReason = learningReason,
+            profileCompleted = profileCompleted,
+            experienceLevel = experienceLevel,
+            lessonsCompleted = lessonsCompleted,
+            highScoreDaysInARow = highScoreDaysInARow,
+            highScoreCorrectAnswersInARow = highScoreCorrectAnswersInARow
+        )
     }
 }
