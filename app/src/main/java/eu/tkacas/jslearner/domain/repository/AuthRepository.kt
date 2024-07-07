4d8efcc65f495a03aec6422545912c3505cb5ab8
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-07 03:20:31 +0300

    - refactor

--- app/src/main/java/eu/tkacas/jslearner/domain/repository/AuthRepository.kt
+++ app/src/main/java/eu/tkacas/jslearner/domain/repository/AuthRepository.kt
@@ -1,15 +1,43 @@
 package eu.tkacas.jslearner.domain.repository
 
 import com.google.firebase.auth.FirebaseUser
+import eu.tkacas.jslearner.data.model.Lesson
+import eu.tkacas.jslearner.data.model.UserFirebase
 import eu.tkacas.jslearner.domain.Result
 import eu.tkacas.jslearner.domain.model.experience.ExperienceLevel
 import eu.tkacas.jslearner.domain.model.learningreason.LearningReason
 
 interface AuthRepository {
     val currentUser: FirebaseUser?
-    suspend fun login(email: String, password: String): Result<FirebaseUser>
-    suspend fun signup(firstName: String, lastName: String, email: String, password: String): Result<FirebaseUser>
-    suspend fun updateUserProfile(learningReason: LearningReason, profileCompleted: Boolean, experienceLevel: ExperienceLevel)
+    suspend fun login(
+        email: String,
+        password: String
+    ): Result<FirebaseUser>
+    suspend fun signup(
+        firstName: String,
+        lastName: String,
+        email: String,
+        password: String
+    ): Result<FirebaseUser>
+    suspend fun updateUserProfile(
+        firstName: String?,
+        lastName: String?,
+        experienceScore: Int?,
+        learningReason: LearningReason?,
+        profileCompleted: Boolean?,
+        experienceLevel: ExperienceLevel?,
+        lessonsCompleted: List<Lesson>?,
+        highScoreDaysInARow: Int?,
+        highScoreCorrectAnswersInARow: Int?
+    )
+    suspend fun updateUserStats(
+        experienceLevel: ExperienceLevel?,
+        experienceScore: Int?,
+        currentCourseId: String?,
+        currentLessonId: String?,
+        highScoreDaysInARow: Int?,
+        highScoreCorrectAnswersInARow: Int?
+    )
     suspend fun checkUserProfileCompletion(): Boolean
     fun logout()
 }
