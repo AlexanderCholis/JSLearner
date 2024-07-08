99291abffbffe835aa73a7ba49a89efee42c7a12
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-08 17:27:04 +0300

    - update use case

--- app/src/main/java/eu/tkacas/jslearner/domain/usecase/user/SetUserStatsUseCase.kt
+++ app/src/main/java/eu/tkacas/jslearner/domain/usecase/user/SetUserStatsUseCase.kt
@@ -2,17 +2,32 @@ package eu.tkacas.jslearner.domain.usecase.user
 
 import eu.tkacas.jslearner.domain.Result
 import eu.tkacas.jslearner.domain.model.User
+import eu.tkacas.jslearner.domain.model.experience.ExperienceLevel
 import eu.tkacas.jslearner.domain.repository.AuthRepository
 
 class SetUserStatsUseCase(private val authRepository: AuthRepository) {
     suspend fun execute(user: User): Result<Unit> {
+        val stats = when (user.experienceLevel) {
+            ExperienceLevel.NO_EXPERIENCE -> UserStats(0, "01", "01", 0, 0)
+            ExperienceLevel.SOME_EXPERIENCE -> UserStats(500, "06", "01", 0, 0)
+            ExperienceLevel.A_LOT_OF_EXPERIENCE -> UserStats(1000, "9", "01", 0, 0)
+            else -> UserStats(user.experienceScore, user.currentCourseId, user.currentLessonId, user.highScoreDaysInARow, user.highScoreCorrectAnswersInARow)
+        }
+
         return authRepository.setUserStats(
             experienceLevel = user.experienceLevel,
-            experienceScore = user.experienceScore,
-            currentCourseId = user.currentCourseId,
-            currentLessonId = user.currentLessonId,
-            highScoreDaysInARow = user.highScoreDaysInARow,
-            highScoreCorrectAnswersInARow = user.highScoreCorrectAnswersInARow
+            experienceScore = stats.experienceScore,
+            currentCourseId = stats.currentCourseId,
+            currentLessonId = stats.currentLessonId,
+            highScoreDaysInARow = stats.highScoreDaysInARow,
+            highScoreCorrectAnswersInARow = stats.highScoreCorrectAnswersInARow
         )
     }
+    data class UserStats(
+        val experienceScore: Int?,
+        val currentCourseId: String?,
+        val currentLessonId: String?,
+        val highScoreDaysInARow: Int?,
+        val highScoreCorrectAnswersInARow: Int?
+    )
 }
