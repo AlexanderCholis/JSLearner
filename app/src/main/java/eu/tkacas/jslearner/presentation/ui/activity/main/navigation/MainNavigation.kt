998d1bad2e5e0a08478f99c654cc19e611b70d2f
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-08 22:07:21 +0300

    - add StartDescriptionViewModel

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/main/navigation/MainNavigation.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/main/navigation/MainNavigation.kt
@@ -8,11 +8,14 @@ import eu.tkacas.jslearner.presentation.ui.activity.main.screens.AccountScreen
 import eu.tkacas.jslearner.presentation.ui.activity.main.screens.LeaderboardScreen
 import eu.tkacas.jslearner.presentation.ui.activity.main.screens.RoadMapScreen
 import eu.tkacas.jslearner.presentation.ui.activity.main.screens.SettingsScreen
+import eu.tkacas.jslearner.presentation.ui.activity.main.screens.StartDescriptionScreen
 import eu.tkacas.jslearner.presentation.viewmodel.main.RoadMapViewModel
+import eu.tkacas.jslearner.presentation.viewmodel.main.StartDescriptionViewModel
 
 @Composable
 internal fun MainNavigation(
-    roadMapViewModel: RoadMapViewModel
+    roadMapViewModel: RoadMapViewModel,
+    startDescriptionViewModel: StartDescriptionViewModel
 ) {
     val navController = rememberNavController()
 
@@ -20,10 +23,35 @@ internal fun MainNavigation(
         navController,
         startDestination = "roadmap"
     ) {
-        composable("roadmap") { RoadMapScreen(navController = navController, viewModel = roadMapViewModel) }
-        composable("account") { AccountScreen(navController = navController) }
-        composable("settings") { SettingsScreen(navController = navController) }
-        composable("leaderboard") { LeaderboardScreen(navController = navController) }
+        composable("roadmap") {
+            RoadMapScreen(
+                navController = navController,
+                viewModel = roadMapViewModel
+            )
+        }
+        composable("startDescription/{id}") { backStackEntry ->
+            val id = backStackEntry.arguments?.getString("id") ?: ""
+            StartDescriptionScreen(
+                navController = navController,
+                viewModel = startDescriptionViewModel,
+                id = id
+            )
+        }
+        composable("account") {
+            AccountScreen(
+                navController = navController
+            )
+        }
+        composable("settings") {
+            SettingsScreen(
+                navController = navController
+            )
+        }
+        composable("leaderboard") {
+            LeaderboardScreen(
+                navController = navController
+            )
+        }
     }
 
 }
