0eb8ed919cef955133c8069204a96ebd8a1a1c2a
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-07-08 17:19:19 +0300

    Navigate from RoadMapScreen to CoursesPathScreen

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/main/navigation/MainNavigation.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/main/navigation/MainNavigation.kt
@@ -5,6 +5,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import eu.tkacas.jslearner.presentation.ui.activity.main.screens.AccountScreen
+import eu.tkacas.jslearner.presentation.ui.activity.main.screens.CoursesPathScreen
 import eu.tkacas.jslearner.presentation.ui.activity.main.screens.LeaderboardScreen
 import eu.tkacas.jslearner.presentation.ui.activity.main.screens.RoadMapScreen
 import eu.tkacas.jslearner.presentation.ui.activity.main.screens.SettingsScreen
@@ -24,6 +25,7 @@ internal fun MainNavigation(
         composable("account") { AccountScreen(navController = navController) }
         composable("settings") { SettingsScreen(navController = navController) }
         composable("leaderboard") { LeaderboardScreen(navController = navController) }
+        composable("coursesPath") { CoursesPathScreen(navController = navController) }
     }
 
 }
