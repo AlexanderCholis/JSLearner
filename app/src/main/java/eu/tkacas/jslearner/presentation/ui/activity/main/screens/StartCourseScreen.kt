c279b010e483b18129be3b678063e8a9e5bfcf49
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-07-08 23:41:59 +0300

    Update UI for StartCourseScreen and StartLessonScreen

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/main/screens/StartCourseScreen.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/activity/main/screens/StartCourseScreen.kt
@@ -1,15 +1,42 @@
 package eu.tkacas.jslearner.presentation.ui.activity.main.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.navigation.NavController
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import eu.tkacas.jslearner.R
 import eu.tkacas.jslearner.data.model.Course
 import eu.tkacas.jslearner.presentation.ui.component.ProgressIndicatorComponent
 import eu.tkacas.jslearner.presentation.viewmodel.main.StartCourseViewModel
 import eu.tkacas.jslearner.domain.Result
+import eu.tkacas.jslearner.presentation.ui.component.BackAppTopBar
+import eu.tkacas.jslearner.presentation.ui.component.GeneralButtonComponent
+import eu.tkacas.jslearner.presentation.ui.theme.componentShapes
 
 
 @Composable
@@ -24,17 +51,119 @@ fun StartCourseScreen(
         viewModel.loadCourse(id)
     }
 
-    when (uiState) {
+    when(uiState) {
+        is Result.Loading -> {
+            ProgressIndicatorComponent()
+        }
         is Result.Success -> {
             val course = (uiState as Result.Success<Course>).result
-            Text(text = "Course: ${course.title}")
+            Scaffold(
+                modifier = Modifier
+                    .fillMaxSize(),
+                topBar = {
+                    BackAppTopBar(
+                        color = Color.White,
+                        onBackClick = {
+                            navController.popBackStack(navController.graph.startDestinationId, inclusive = false)
+                            navController.navigate("roadmap")
+                        },
+                        title = course.title
+                    )
+                }
+            ) { innerPadding ->
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.White)
+                        .padding(innerPadding)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        Surface(
+                            color = Color.Gray,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .clip(componentShapes.medium)
+                        ) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(top = 16.dp, start = 36.dp, end = 36.dp, bottom = 16.dp)
+                            ) {
+                                /*subsections.forEach {
+                                    BulletText(value = it)
+                                }*/ //TODO: add Lessons
+                                Spacer(modifier = Modifier.height(100.dp))
+
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.application),
+                                        contentDescription = stringResource(id = R.string.simple_image),
+                                        modifier = Modifier.size(240.dp)
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(48.dp))
+
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    Text(
+                                        text = course.descriptionShort,
+                                        fontSize = 16.sp,
+                                        fontWeight = FontWeight.Bold,
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                                Column(
+                                    modifier = Modifier.fillMaxSize(),
+                                    verticalArrangement = Arrangement.Bottom,
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    GeneralButtonComponent(
+                                        valueId = R.string.start_course,
+                                        onButtonClicked = {
+                                            //TODO: navigate to the lesson
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
         is Result.Error -> {
-            val error = (uiState as Result.Error).exception
-            error.message?.let { Text(text = it) }
-        }
-        Result.Loading -> {
-            ProgressIndicatorComponent()
+            Scaffold(
+                modifier = Modifier
+                    .fillMaxSize(),
+                topBar = {
+                    BackAppTopBar(
+                        color = Color.White,
+                        onBackClick = {
+                            navController.popBackStack(navController.graph.startDestinationId, inclusive = false)
+                            navController.navigate("roadmap")
+                        }
+                    )
+                }
+            ) { innerPadding ->
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.White)
+                        .padding(innerPadding)
+                ) {
+                    val error = (uiState as Result.Error).exception
+                    Text(text = error.message ?: "An error occurred")
+                }
+            }
         }
     }
 }
