1980de51b6a587cbbd87f3ee718083c6461667db
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-09 11:58:59 +0300

    - load courses too

--- app/src/main/java/eu/tkacas/jslearner/presentation/viewmodel/main/StartCourseViewModel.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/viewmodel/main/StartCourseViewModel.kt
@@ -2,9 +2,10 @@ package eu.tkacas.jslearner.presentation.viewmodel.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import eu.tkacas.jslearner.data.model.Course
 import eu.tkacas.jslearner.domain.Result
+import eu.tkacas.jslearner.domain.model.roadmap.CourseWithLessons
 import eu.tkacas.jslearner.domain.usecase.main.roadmap.GetCourseUseCase
+import eu.tkacas.jslearner.domain.usecase.main.roadmap.GetLessonsUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -13,20 +14,22 @@ import kotlinx.coroutines.launch
 
 class StartCourseViewModel(
     private val getCourseUseCase: GetCourseUseCase,
+    private val getLessonsUseCase: GetLessonsUseCase
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<Result<Course>>(Result.Loading)
-    val uiState: StateFlow<Result<Course>> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow<Result<CourseWithLessons>>(Result.Loading)
+    val uiState: StateFlow<Result<CourseWithLessons>> = _uiState.asStateFlow()
 
-   fun loadCourse(courseId: String) {
-       viewModelScope.launch(Dispatchers.IO) {
-           try {
-               _uiState.value = Result.Loading
+    fun loadCourse(courseId: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                _uiState.value = Result.Loading
                 val course = getCourseUseCase.execute(courseId)
-                _uiState.value = Result.Success(course)
-           } catch (e: Exception) {
-               _uiState.value = Result.Error(e)
-           }
-       }
-   }
+                val lessons = getLessonsUseCase.execute(courseId)
+                _uiState.value = Result.Success(CourseWithLessons(course, lessons))
+            } catch (e: Exception) {
+                _uiState.value = Result.Error(e)
+            }
+        }
+    }
 }
