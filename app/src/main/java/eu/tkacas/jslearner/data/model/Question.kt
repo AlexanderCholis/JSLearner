bd92641e7cb964b294d44fe9c7879df5583dfe80
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-10 19:23:03 +0300

    - update question type

--- app/src/main/java/eu/tkacas/jslearner/data/model/Question.kt
+++ app/src/main/java/eu/tkacas/jslearner/data/model/Question.kt
@@ -6,6 +6,6 @@ data class Question(
     @PropertyName("question_type") val questionType: String = "",
     @PropertyName("hint") val hint: String = "",
     @PropertyName("question_description") val questionDescription: String = "",
-    @PropertyName("options") val options: List<String> = emptyList(),
-    @PropertyName("correct_answers") val correctAnswers: List<String> = emptyList()
+    @PropertyName("options") val options: List<Map<String, String>> = emptyList(),
+    @PropertyName("correct_answers") val correctAnswers: List<Map<String, String>> = emptyList()
 )
