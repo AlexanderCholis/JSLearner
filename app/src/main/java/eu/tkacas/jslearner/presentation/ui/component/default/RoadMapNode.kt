6b653b5d8b9006009e64a63a26f2d28fe5275eac
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-10 13:09:41 +0300

    - modify messageBubble to change the course look

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/component/default/RoadMapNode.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/component/default/RoadMapNode.kt
@@ -1,9 +1,11 @@
 package eu.tkacas.jslearner.presentation.ui.component.default
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,12 +13,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,10 +34,15 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import eu.tkacas.jslearner.R
 import eu.tkacas.jslearner.domain.model.roadmap.CircleParameters
 import eu.tkacas.jslearner.domain.model.roadmap.LineParameters
 import eu.tkacas.jslearner.domain.model.roadmap.RoadMapNodeCategory
@@ -42,6 +52,7 @@ import eu.tkacas.jslearner.domain.model.roadmap.RoadMapNodeStatus
 import eu.tkacas.jslearner.domain.model.roadmap.StrokeParameters
 import eu.tkacas.jslearner.domain.model.roadmap.getColor
 import eu.tkacas.jslearner.domain.model.roadmap.getIcon
+import eu.tkacas.jslearner.domain.model.roadmap.getTextColor
 
 
 @Composable
@@ -110,7 +121,7 @@ fun RoadMapNode(
                     start = circleParameters.radius * 2 + contentStartOffset,
                     bottom = if (nodeState.position != RoadMapNodePosition.LAST) spacer else 0.dp
                 )
-                .offset (y = -circleParameters.radius / 3)
+                .offset(y = -circleParameters.radius / 3)
         )
     }
 }
@@ -141,6 +152,7 @@ fun RoadMapNodePreview() {
             RoadMapNode(
                 nodeState = node,
                 circleParameters = CircleParametersDefaults.circleParameters(
+                    category = node.category,
                     backgroundColor = node.status.getColor(),
                     stroke = StrokeParameters(color = node.status.getColor(), width = 2.dp),
                     icon = node.status.getIcon()
@@ -149,6 +161,7 @@ fun RoadMapNodePreview() {
                 content = { modifier ->
                     node.title?.let {
                         MessageBubble(
+                            nodeState = node,
                             modifier,
                             containerColor = node.status.getColor(),
                             text = it,
@@ -164,23 +177,59 @@ fun RoadMapNodePreview() {
 }
 
 @Composable
-fun MessageBubble(modifier: Modifier, containerColor: Color, text: String, onClick: () -> Unit) {
+fun MessageBubble(
+    nodeState: RoadMapNodeState,
+    modifier: Modifier = Modifier,
+    containerColor: Color,
+    text: String,
+    onClick: () -> Unit,
+) {
+    var borderColor: Color = Color.Transparent
+    var borderWidth: Dp = 0.dp
+    var fontSize = 15.sp
+    var fontWeight = FontWeight.Normal
+    var fontStyle = FontStyle.Normal
+    var iconRes: Int? = null
+
+    if (nodeState.category == RoadMapNodeCategory.COURSE) {
+        borderColor = Color.Black
+        borderWidth = 3.dp
+        fontSize = 18.sp
+        fontWeight = FontWeight.Bold
+        fontStyle = FontStyle.Normal
+        iconRes = R.drawable.bookmark
+    }
+
     Card(
         modifier = modifier
             .width(300.dp)
             .height(30.dp)
-            .clickable(onClick = onClick),
+            .clickable(onClick = onClick)
+            .border(width = borderWidth, color = borderColor, shape = MaterialTheme.shapes.medium),
         colors = CardDefaults.cardColors(containerColor = containerColor)
     ) {
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = text,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp), // added padding
-            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                iconRes?.let {
+                    Icon(
+                        painter = painterResource(id = it),
+                        contentDescription = stringResource(R.string.bookmark_icon),
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+                Text(
+                    text = text,
+                    color = nodeState.status.getTextColor(),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp),
+                    fontSize = fontSize,
+                    fontWeight = fontWeight,
+                    fontStyle = fontStyle
+                )
+            }
         }
     }
 }
