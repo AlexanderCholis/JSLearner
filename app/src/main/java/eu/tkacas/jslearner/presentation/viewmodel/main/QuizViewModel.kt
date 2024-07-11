7f0c92a8959aa91c01ed948645a3dd9676c256c3
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-11 13:09:57 +0300

    - updated quizViewModel

--- app/src/main/java/eu/tkacas/jslearner/presentation/viewmodel/main/QuizViewModel.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/viewmodel/main/QuizViewModel.kt
@@ -1,19 +1,31 @@
 package eu.tkacas.jslearner.presentation.viewmodel.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import eu.tkacas.jslearner.domain.Result
-import eu.tkacas.jslearner.domain.usecase.main.roadmap.GetQuestionsUseCase
+import eu.tkacas.jslearner.domain.model.Quiz
+import eu.tkacas.jslearner.domain.usecase.main.roadmap.GetQuizUseCase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class QuizViewModel (
-    getQuestionsUseCase: GetQuestionsUseCase
+    private val getQuizUseCase: GetQuizUseCase
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow<Result<Quiz>>(Result.Loading)
-    val uiState: StateFlow<Result<Quiz>> = _uiState //TODO: Add Quiz model
+    val uiState: StateFlow<Result<Quiz>> = _uiState
 
-    fun loadQuiz(id: String) { //TODO: Retrieve Quiz Model
-
+    fun loadQuiz(lessonId: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                _uiState.value = Result.Loading
+                val quiz = getQuizUseCase.execute(lessonId)
+                _uiState.value = Result.Success(quiz)
+            } catch (e: Exception) {
+                _uiState.value = Result.Error(e)
+            }
+        }
     }
 }
