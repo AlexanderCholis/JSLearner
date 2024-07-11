efcaf313a0fcb52acb1179a3a52f10a3084cfe47
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-11 19:11:20 +0300

    - change Question fields

--- app/src/main/java/eu/tkacas/jslearner/data/model/Question.kt
+++ app/src/main/java/eu/tkacas/jslearner/data/model/Question.kt
@@ -3,9 +3,9 @@ package eu.tkacas.jslearner.data.model
 import com.google.firebase.firestore.PropertyName
 
 data class Question(
-    @PropertyName("question_type") val questionType: String = "",
+    @PropertyName("question_type") val questionType: QuestionType? = null,
     @PropertyName("hint") val hint: String = "",
     @PropertyName("question_description") val questionDescription: String = "",
-    @PropertyName("options") val options: List<Map<String, String>> = emptyList(),
-    @PropertyName("correct_answers") val correctAnswers: List<Map<String, String>> = emptyList()
+    @PropertyName("options") val options: Any = emptyList<String>(),
+    @PropertyName("correct_answers") val correctAnswers: List<String> = emptyList()
 )
