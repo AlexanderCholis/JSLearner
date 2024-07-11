3eb748e2bcb90f3bd21cbaf81156218eca0498ae
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-11 13:09:35 +0300

    - rename GetQuestionsUseCase.kt

---
+++ app/src/main/java/eu/tkacas/jslearner/domain/usecase/main/roadmap/GetQuizUseCase.kt
@@ -0,0 +1,13 @@
+package eu.tkacas.jslearner.domain.usecase.main.roadmap
+
+import eu.tkacas.jslearner.data.model.Question
+import eu.tkacas.jslearner.domain.model.Quiz
+import eu.tkacas.jslearner.domain.repository.RoadMapRepository
+
+class GetQuizUseCase(private val roadMapRepository: RoadMapRepository) {
+    suspend fun execute(lessonId: String): Quiz {
+        val courseId = lessonId.substring(0, 2)
+        val questions = roadMapRepository.getQuestions(courseId, lessonId)
+        return Quiz(questions, 0)
+    }
+}
