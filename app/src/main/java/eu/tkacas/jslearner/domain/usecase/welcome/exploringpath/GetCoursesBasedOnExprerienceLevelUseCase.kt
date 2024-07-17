b2c6e4667de0e441bf7368394f01332864e8336e
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-17 11:52:58 +0300

    - update the GetCoursesBasedOnExperienceUseCase

--- app/src/main/java/eu/tkacas/jslearner/domain/usecase/welcome/exploringpath/GetCoursesBasedOnExprerienceLevelUseCase.kt
+++ app/src/main/java/eu/tkacas/jslearner/domain/usecase/welcome/exploringpath/GetCoursesBasedOnExprerienceLevelUseCase.kt
@@ -1,12 +1,18 @@
 package eu.tkacas.jslearner.domain.usecase.welcome.exploringpath
 
+import eu.tkacas.jslearner.data.model.CourseLevel
 import eu.tkacas.jslearner.domain.model.CourseShort
 import eu.tkacas.jslearner.domain.model.experience.ExperienceLevel
-import eu.tkacas.jslearner.domain.repository.ExploringPathRepository
+import eu.tkacas.jslearner.domain.repository.RoadMapRepository
 
-class GetCoursesBasedOnExperienceUseCase(private val exploringPathRepository: ExploringPathRepository) {
+class GetCoursesBasedOnExperienceUseCase(private val roadMapRepository: RoadMapRepository) {
     suspend fun execute(experienceLevel: ExperienceLevel): MutableList<CourseShort> {
-        val courses = exploringPathRepository.getCoursesBasedOnExperience(experienceLevel)
+
+        val courses = when (experienceLevel) {
+            ExperienceLevel.NO_EXPERIENCE -> roadMapRepository.getCoursesBasedOnLevel(CourseLevel.BEGINNER)
+            ExperienceLevel.SOME_EXPERIENCE -> roadMapRepository.getCoursesBasedOnLevel(CourseLevel.INTERMEDIATE)
+            ExperienceLevel.A_LOT_OF_EXPERIENCE -> roadMapRepository.getCoursesBasedOnLevel(CourseLevel.ADVANCED)
+        }
 
         val coursesList = mutableListOf<CourseShort>()
 
