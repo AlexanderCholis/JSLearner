a1ed91412945d2c8cef94685e23ddea9a0fb8437
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-07-18 22:34:51 +0300

    Update ResultLayout Ui

--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/component/quiz/ResultLayout.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/component/quiz/ResultLayout.kt
@@ -3,17 +3,22 @@ package eu.tkacas.jslearner.presentation.ui.component.quiz
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -38,16 +43,24 @@ fun ResultLayout(
     totalScore: Int,
     onQuestionSelected: (Int) -> Unit
 ) {
-    Column {
-        DisplayScore(totalScore, questions)
-        LazyColumn {
-            itemsIndexed(questions) { index, question ->
-                QuestionItem(
-                    questionNumber = index + 1,
-                    isCorrect = question.score,
-                    onQuestionSelected = { onQuestionSelected(index) }
-                )
+    Box {
+        Column(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(horizontal = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            DisplayScore(totalScore, questions)
+            LazyColumn {
+                itemsIndexed(questions) { index, question ->
+                    QuestionItem(
+                        questionNumber = index + 1,
+                        isCorrect = question.score,
+                        onQuestionSelected = { onQuestionSelected(index) }
+                    )
+                }
             }
+
         }
     }
 }
@@ -109,19 +122,6 @@ fun DisplayScore(
         Spacer(modifier = Modifier.height(16.dp))
         if (correctAnswers >= questions.size / 2) {
             Image(
-                painter = painterResource(id = R.drawable.element_teacher_fail),
-                contentDescription = stringResource(id = R.string.fail),
-                modifier = Modifier
-                    .size(200.dp, 200.dp)
-            )
-            Spacer(modifier = Modifier.height(10.dp))
-            Text(
-                text = stringResource(id = R.string.you_can_do_better),
-                fontSize = 16.sp,
-                color = SkyBlue
-            )
-        } else {
-            Image(
                 painter = painterResource(id = R.drawable.element_teacher_pass),
                 contentDescription = stringResource(id = R.string.pass),
                 modifier = Modifier
@@ -130,6 +130,19 @@ fun DisplayScore(
             Spacer(modifier = Modifier.height(10.dp))
             Text(
                 text = stringResource(id = R.string.great_job),
+                fontSize = 16.sp,
+                color = SkyBlue
+            )
+        } else {
+            Image(
+                painter = painterResource(id = R.drawable.element_teacher_fail),
+                contentDescription = stringResource(id = R.string.fail),
+                modifier = Modifier
+                    .size(200.dp, 200.dp)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = stringResource(id = R.string.you_can_do_better),
                 fontSize = 16.sp,
                 color = SkyBlue
             )
