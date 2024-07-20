aebec30a7c830b9fb05170b19f4bc419383024cc
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-20 12:23:15 +0300

    - rename score to isCorrect

--- app/src/main/java/eu/tkacas/jslearner/domain/model/quiz/QuestionResult.kt
+++ app/src/main/java/eu/tkacas/jslearner/domain/model/quiz/QuestionResult.kt
@@ -3,5 +3,5 @@ package eu.tkacas.jslearner.domain.model.quiz
 data class QuestionResult(
     val correctOptions: List<String>,
     val wrongOptions: List<String>,
-    val score: Boolean
+    val isCorrect: Boolean
 )
--- app/src/main/java/eu/tkacas/jslearner/presentation/ui/component/quiz/ResultLayout.kt
+++ app/src/main/java/eu/tkacas/jslearner/presentation/ui/component/quiz/ResultLayout.kt
@@ -3,13 +3,10 @@ package eu.tkacas.jslearner.presentation.ui.component.quiz
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -18,7 +15,6 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -31,7 +27,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import eu.tkacas.jslearner.R
 import eu.tkacas.jslearner.domain.model.quiz.QuestionResult
-import eu.tkacas.jslearner.domain.model.quiz.QuestionUI
 import eu.tkacas.jslearner.presentation.ui.theme.GreenPal
 import eu.tkacas.jslearner.presentation.ui.theme.PrussianBlue
 import eu.tkacas.jslearner.presentation.ui.theme.RedPal
@@ -55,7 +50,7 @@ fun ResultLayout(
                 itemsIndexed(questions) { index, question ->
                     QuestionItem(
                         questionNumber = index + 1,
-                        isCorrect = question.score,
+                        isCorrect = question.isCorrect,
                         onQuestionSelected = { onQuestionSelected(index) }
                     )
                 }
@@ -106,7 +101,7 @@ fun DisplayScore(
     courseScore: Int,
     questions: List<QuestionResult>
 ) {
-    val correctAnswers = questions.count { it.score }
+    val correctAnswers = questions.count { it.isCorrect }
     Column(
         modifier = Modifier.padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
